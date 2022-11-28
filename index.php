e732e73babf00cd689861a91ceb4180ffb799fba
uwecan-bringingSmiles <119356322+uwecan-bringingSmiles@users.noreply.github.com>
2022-11-29 01:11:31 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;600&display=swap" rel="stylesheet">
+    <title>Parivagan Public Challan</title>
+    <link rel='icon' href="images/icon.png">
+    <link rel='stylesheet' href="style.css">
+</head>
+<body>
+    <div class="main">
+        <nav>
+            <img src="images/logo.png" class="logo">
+            <ul>
+                <li><a href="">About</a></li>
+                <li><a href="login.php">Login</a></li>
+                
+            </ul>
+        </nav>
+        <div class="row">
+            <dic class="col-1">
+                <img src="images/bg-image.png">
+            </dic>
+            <dic class="col-2">
+                <h1>No rule, Know pain</h1>
+                <h1>Know rule, No pain<br><span>Safety First</span></h1>
+
+           <div class="btw">
+                <a href="fillchallan.php" class="btn">Fill Challan</a>
+                <a href="" class="btn">Check Status</a>
+
+            </dic>
+</div>
+        </div>
+
+    </div>
+</body>
+</html> 
